ba787f4fecde732793fb95ff3a00dc135abb2b2e
Kathy <kleboeuf@townisp.com>
2018-05-15 17:26:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
+function holidayCountdown(holiday, dyas) {
+  return `Happy ${holiday}, ${name}!`
+}
